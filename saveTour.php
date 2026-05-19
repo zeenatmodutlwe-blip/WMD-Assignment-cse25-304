b9fbcee0f95e496d5e9c4d0391c88e70352cc508
zeenatmodutlwe-blip <zeenatmodutlwe@gmail.com>
2026-05-19 12:02:52 +0200

    Update saveTour.php to include additional fields

--- saveTour.php
+++ saveTour.php
@@ -1,14 +1,14 @@
 <?php
 
 $fullname = $_POST['fullname'];
-$email = $_POST['email'];
+$address = $_POST['address'];
 $phone = $_POST['phone'];
 $tourdate = $_POST['tourdate'];
+$tourtime = $_POST['tourtime'];
+$bedrooms = $_POST['bedrooms'];
+$comments = $_POST['comments'];
 
-$data = "Name: " . $fullname .
-        " | Email: " . $email .
-        " | Phone: " . $phone .
-        " | Tour Date: " . $tourdate . "\n";
+$data = "Name: $fullname | Address: $address | Phone: $phone | Date: $tourdate | Time: $tourtime | Bedrooms: $bedrooms | Comments: $comments\n";
 
 $file = fopen("tour_bookings.txt", "a");
 
